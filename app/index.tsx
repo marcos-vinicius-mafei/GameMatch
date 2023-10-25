19b2ee1e5ada4ba869df63e14ed5ef7dbdd1561b
marcos <marcosmafeimachado@gmail.com>
2023-10-24 21:51:41 -0300

    feat: created the app icon and now using it in the initial page;

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,45 @@
-import { StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 import React from 'react';
 import Typography from '../components/Typography';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <Typography variant='headlineLarge'>Home</Typography>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inline}>
+        <View>
+          <Typography variant='headlineLarge' fontFamily='SpaceMono'>
+            Game Matcher
+          </Typography>
+          <Typography variant='titleMedium' textColor='primary'>
+            Find your next favorite game
+          </Typography>
+        </View>
+        <Image
+          style={styles.image}
+          source={require('../assets/images/GMAppIcon.png')}
+          resizeMode='contain'
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 20,
+    paddingHorizontal: 16,
+  },
+  inline: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  image: {
+    width: '30%',
+    aspectRatio: 1,
   },
 });
 
--- components/Typography.tsx
+++ components/Typography.tsx
@@ -1,14 +1,26 @@
-import { Text, TextProps } from 'react-native-paper';
+import { Text, TextProps, useTheme } from 'react-native-paper';
 import React from 'react';
-import { Text as NativeText } from 'react-native';
+import { ColorValue, Text as NativeText } from 'react-native';
+import { MD3Colors } from 'react-native-paper/lib/typescript/types';
 
 interface Props extends TextProps<NativeText> {
   fontFamily?: 'RobotoRegular' | 'RobotoMedium' | 'SpaceMono';
+  textColor?: keyof MD3Colors;
 }
 
-const Typography = ({ fontFamily = 'RobotoMedium', ...rest }: Props) => {
+const Typography = ({
+  fontFamily = 'RobotoMedium',
+  textColor = 'onSurface',
+  ...rest
+}: Props) => {
+  const { colors } = useTheme();
   return (
-    <Text {...rest} style={[rest.style, { fontFamily }]}>
+    <Text
+      {...rest}
+      style={[
+        rest.style,
+        { fontFamily, color: colors[textColor] as ColorValue },
+      ]}>
       {rest.children}
     </Text>
   );
