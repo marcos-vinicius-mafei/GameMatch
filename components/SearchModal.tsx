4e1e01d3ef1b56efade78c12b9566e8e7a4a2fbb
marcos <marcosmafeimachado@gmail.com>
2023-10-26 17:57:59 -0300

    feat: created the error container for the SearchModal;

--- components/SearchModal.tsx
+++ components/SearchModal.tsx
@@ -3,14 +3,28 @@ import React, { useCallback } from 'react';
 import Modal, { ModalProps } from './Modal';
 import Typography from './Typography';
 import { BottomSheetView } from '@gorhom/bottom-sheet';
-import { useGameGenres } from '../queries';
+import { useGameGenres, usePlatforms } from '../queries';
 import { Genre } from '../types';
-import { Chip } from 'react-native-paper';
+import { Button, Chip } from 'react-native-paper';
+import FooterLoading from './FooterLoading';
 
 type Props = Pick<ModalProps, 'modalRef'>;
 
 const SearchModal = ({ modalRef }: Props) => {
-  const { data: genresData } = useGameGenres();
+  const {
+    data: genresData,
+    isError: genreError,
+    refetch: refetchGenres,
+    isFetching: isFetchingGenres,
+  } = useGameGenres();
+  const {
+    data: platformsData,
+    isError: platformError,
+    refetch: refetchPlatforms,
+    isFetching: isFetchingPlatforms,
+  } = usePlatforms();
+
+  const isError = genreError || platformError;
 
   const renderGenreItem = useCallback(({ item }: { item: Genre }) => {
     return (
@@ -20,39 +34,79 @@ const SearchModal = ({ modalRef }: Props) => {
     );
   }, []);
 
-  console.log(genresData);
+  function retry() {
+    if (genreError && !isFetchingGenres) {
+      refetchGenres();
+    }
+    if (platformError && !isFetchingPlatforms) {
+      refetchPlatforms();
+    }
+  }
 
   return (
     <Modal modalRef={modalRef} snapPoints={['30%', '50%', '65%']} index={1}>
-      <BottomSheetView>
+      <BottomSheetView style={styles.container}>
         <View style={styles.titleWrapper}>
           <Typography variant='titleLarge' style={styles.title}>
             Select the filters below that match your desired game
           </Typography>
         </View>
-        <ScrollView contentContainerStyle={styles.scrollView}>
-          <View style={styles.sectionContainer}>
-            <Typography
-              variant='titleMedium'
-              textColor='onSurfaceVariant'
-              style={styles.sectionTitle}>
-              Genres
+        {isError ? (
+          <View style={styles.errorContainer}>
+            <Typography variant='headlineMedium' textColor='onSurfaceVariant'>
+              Something went Wrong 😔
             </Typography>
-            <FlatList
-              showsHorizontalScrollIndicator={false}
-              horizontal
-              data={genresData}
-              renderItem={renderGenreItem}
-              contentContainerStyle={styles.sectionList}
-            />
+            <Button
+              mode='contained'
+              style={styles.tryAgainButton}
+              onPress={retry}>
+              Try Again
+            </Button>
           </View>
-        </ScrollView>
+        ) : (
+          <ScrollView contentContainerStyle={styles.scrollView}>
+            <View style={styles.sectionContainer}>
+              <Typography
+                variant='titleMedium'
+                textColor='onSurfaceVariant'
+                style={styles.sectionTitle}>
+                Genres
+              </Typography>
+              <FlatList
+                ListFooterComponent={
+                  isFetchingGenres ? (
+                    <FooterLoading style={styles.footerLoading} />
+                  ) : null
+                }
+                showsHorizontalScrollIndicator={false}
+                horizontal
+                data={genresData}
+                renderItem={renderGenreItem}
+                contentContainerStyle={styles.sectionList}
+              />
+            </View>
+          </ScrollView>
+        )}
       </BottomSheetView>
     </Modal>
   );
 };
 
 const styles = StyleSheet.create({
+  footerLoading: {
+    marginHorizontal: 8,
+  },
+  container: {
+    flex: 1,
+  },
+  errorContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 32,
+  },
+  tryAgainButton: {
+    marginTop: 8,
+  },
   titleWrapper: {
     width: '100%',
     alignItems: 'center',
